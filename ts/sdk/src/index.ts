395165138303e02551047d1b1a99944780d3f0f9
Chris Heaney <chrisheaney30@gmail.com>
2023-06-11 16:18:32 -0400

    add jitProxyClient

---
+++ ts/sdk/src/index.ts
@@ -0,0 +1,1 @@
+export * from './types/jit_proxy';
---
+++ ts/sdk/src/jitProxyClient.ts
@@ -0,0 +1,88 @@
+import {
+	BN,
+	DriftClient,
+	isVariant,
+	PostOnlyParams,
+	QUOTE_SPOT_MARKET_INDEX,
+	TxParams,
+	UserAccount,
+} from '@drift-labs/sdk';
+import { IDL, JitProxy } from './types/jit_proxy';
+import { PublicKey, TransactionInstruction } from '@solana/web3.js';
+import { Program } from '@coral-xyz/anchor';
+import { TxSigAndSlot } from '@drift-labs/sdk/lib/tx/types';
+
+export type JitIxParams = {
+	takerKey: PublicKey;
+	takerStatsKey: PublicKey;
+	taker: UserAccount;
+	takerOrderId: number;
+	maxPosition: BN;
+	worstPrice: BN;
+	postOnly: PostOnlyParams | null;
+};
+
+export class JitProxyClient {
+	private driftClient: DriftClient;
+	private program: Program<JitProxy>;
+
+	constructor({
+		driftClient,
+		programId,
+	}: {
+		driftClient: DriftClient;
+		programId: PublicKey;
+	}) {
+		this.driftClient = driftClient;
+		this.program = new Program(IDL, programId, driftClient.provider);
+	}
+
+	public async jit(
+		params: JitIxParams,
+		txParams?: TxParams
+	): Promise<TxSigAndSlot> {
+		const ix = await this.getJitIx(params);
+		const tx = await this.driftClient.buildTransaction([ix], txParams);
+		return await this.driftClient.sendTransaction(tx);
+	}
+
+	public async getJitIx({
+		takerKey,
+		takerStatsKey,
+		taker,
+		takerOrderId,
+		maxPosition,
+		worstPrice,
+		postOnly = null,
+	}: JitIxParams): Promise<TransactionInstruction> {
+		const order = taker.orders.find((order) => order.orderId === takerOrderId);
+		const remainingAccounts = this.driftClient.getRemainingAccounts({
+			userAccounts: [taker, this.driftClient.getUserAccount()],
+			writableSpotMarketIndexes: isVariant(order.marketType, 'spot')
+				? [order.marketIndex, QUOTE_SPOT_MARKET_INDEX]
+				: [],
+			writablePerpMarketIndexes: isVariant(order.marketType, 'perp')
+				? [order.marketIndex]
+				: [],
+		});
+
+		const jitParams = {
+			takerOrderId,
+			maxPosition,
+			worstPrice,
+			postOnly,
+		};
+
+		return this.program.methods
+			.jit(jitParams)
+			.accounts({
+				taker: takerKey,
+				takerStats: takerStatsKey,
+				state: await this.driftClient.getStatePublicKey(),
+				user: await this.driftClient.getUserAccountPublicKey(),
+				userStats: this.driftClient.getUserStatsAccountPublicKey(),
+			})
+			.remainingAccounts(remainingAccounts)
+			.instruction();
+	}
+}
